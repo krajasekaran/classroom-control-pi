547405b7ee169c04c6077563ae692f1c2faf1cec
Laura Macchi <lau.macchi@gmail.com>
2016-08-23 13:05:37 -0500

    Update site.pp
    Added an exact node definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,10 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'laura.puppetlabs.vm' {
+  notify { "This is an exact match in site.pp": }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
